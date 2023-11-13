a544a2ec92f19ea3db0a6363e9fa6526c4c6650e
jiboladestiny <jibola1444@gmail.com>
2023-11-13 12:04:48 +0000

    make modification to code

--- helpers/getAllEnrolledCourse.ts
+++ helpers/getAllEnrolledCourse.ts
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const enrolledCourseData = async () => {
-    try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/enroll`);
-        return res.data;
-    } catch (error) {
-
-        throw new Error("failed to fetchdata");
-    }
+    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/enroll`)
+    if (!res.ok) throw new Error("failed to fetchdata");
+    return res.json();  
 };
 
 export default enrolledCourseData;
