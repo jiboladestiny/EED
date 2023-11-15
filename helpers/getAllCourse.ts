a3b7be42929e6dd646d4d663dbc70d2d98aabf42
jiboladestiny <jibola1444@gmail.com>
2023-11-15 17:48:02 +0000

    refreshing

--- helpers/getAllCourse.ts
+++ helpers/getAllCourse.ts
@@ -1,10 +1,11 @@
-
-
 const courseData = async () => {
-
-    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/course`);
-    if (!res.ok) throw new Error("failed to fetchdata");
-    return res.json();
+    return fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/course`)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("failed to fetch data");
+            }
+            return res.json();
+        });
 };
 
-export default courseData
+export default courseData;
